7d96e8a29b63360a13ba5b03a26bdb6a2a5ac4f3
NtsDK <ghoul71@yandex.ru>
2016-02-29 05:45:46 +0500

    Fixed many bugs in 3D rendering. Added support of loading data from vis.js library from DataSets.

--- drawings/simple_graph.js
+++ drawings/simple_graph.js
@@ -45,6 +45,34 @@
   Feel free to contribute a new drawing!
 
  */
+ 
+var colorPalette = [
+      //{color: {border: "#2B7CE9", background: "#97C2FC", highlight: {border: "#2B7CE9", background: "#D2E5FF"}, hover: {border: "#2B7CE9", background: "#D2E5FF"}}}, // 0: blue
+      //{color: {border: "#FFA500", background: "#FFFF00", highlight: {border: "#FFA500", background: "#FFFFA3"}, hover: {border: "#FFA500", background: "#FFFFA3"}}}, // 1: yellow
+      {color: {border: "#FA0A10", background: "#FB7E81", highlight: {border: "#FA0A10", background: "#FFAFB1"}, hover: {border: "#FA0A10", background: "#FFAFB1"}}}, // 2: red
+      {color: {border: "#41A906", background: "#7BE141", highlight: {border: "#41A906", background: "#A1EC76"}, hover: {border: "#41A906", background: "#A1EC76"}}}, // 3: green
+      {color: {border: "#E129F0", background: "#EB7DF4", highlight: {border: "#E129F0", background: "#F0B3F5"}, hover: {border: "#E129F0", background: "#F0B3F5"}}}, // 4: magenta
+      {color: {border: "#7C29F0", background: "#AD85E4", highlight: {border: "#7C29F0", background: "#D3BDF0"}, hover: {border: "#7C29F0", background: "#D3BDF0"}}}, // 5: purple
+      {color: {border: "#C37F00", background: "#FFA807", highlight: {border: "#C37F00", background: "#FFCA66"}, hover: {border: "#C37F00", background: "#FFCA66"}}}, // 6: orange
+      {color: {border: "#4220FB", background: "#6E6EFD", highlight: {border: "#4220FB", background: "#9B9BFD"}, hover: {border: "#4220FB", background: "#9B9BFD"}}}, // 7: darkblue
+      {color: {border: "#FD5A77", background: "#FFC0CB", highlight: {border: "#FD5A77", background: "#FFD1D9"}, hover: {border: "#FD5A77", background: "#FFD1D9"}}}, // 8: pink
+      {color: {border: "#4AD63A", background: "#C2FABC", highlight: {border: "#4AD63A", background: "#E6FFE3"}, hover: {border: "#4AD63A", background: "#E6FFE3"}}}, // 9: mint
+      
+      {color: {border: "#990000", background: "#EE0000", highlight: {border: "#BB0000", background: "#FF3333"}, hover: {border: "#BB0000", background: "#FF3333"}}}, // 10:bright red
+      
+      {color: {border: "#FF6000", background: "#FF6000", highlight: {border: "#FF6000", background: "#FF6000"}, hover: {border: "#FF6000", background: "#FF6000"}}}, // 12: real orange
+      {color: {border: "#97C2FC", background: "#2B7CE9", highlight: {border: "#D2E5FF", background: "#2B7CE9"}, hover: {border: "#D2E5FF", background: "#2B7CE9"}}}, // 13: blue
+      {color: {border: "#399605", background: "#255C03", highlight: {border: "#399605", background: "#255C03"}, hover: {border: "#399605", background: "#255C03"}}}, // 14: green
+      {color: {border: "#B70054", background: "#FF007E", highlight: {border: "#B70054", background: "#FF007E"}, hover: {border: "#B70054", background: "#FF007E"}}}, // 15: magenta
+      {color: {border: "#AD85E4", background: "#7C29F0", highlight: {border: "#D3BDF0", background: "#7C29F0"}, hover: {border: "#D3BDF0", background: "#7C29F0"}}}, // 16: purple
+      {color: {border: "#4557FA", background: "#000EA1", highlight: {border: "#6E6EFD", background: "#000EA1"}, hover: {border: "#6E6EFD", background: "#000EA1"}}}, // 17: darkblue
+      {color: {border: "#FFC0CB", background: "#FD5A77", highlight: {border: "#FFD1D9", background: "#FD5A77"}, hover: {border: "#FFD1D9", background: "#FD5A77"}}}, // 18: pink
+      {color: {border: "#C2FABC", background: "#74D66A", highlight: {border: "#E6FFE3", background: "#74D66A"}, hover: {border: "#E6FFE3", background: "#74D66A"}}}, // 19: mint
+      
+      {color: {border: "#EE0000", background: "#990000", highlight: {border: "#FF3333", background: "#BB0000"}, hover: {border: "#FF3333", background: "#BB0000"}}} // 20:bright red
+    ];
+		
+		var groupColors = {};
 
 var Drawing = Drawing || {};
 
@@ -102,7 +130,7 @@ Drawing.SimpleGraph = function(options) {
 
     // Node geometry
     if(that.layout === "3d") {
-      geometry = new THREE.CubeGeometry( 25, 25, 25 );
+      geometry = new THREE.SphereGeometry( 50, 50, 50 );
     } else {
       geometry = new THREE.CubeGeometry( 50, 50, 0 );
     }
@@ -151,34 +179,67 @@ Drawing.SimpleGraph = function(options) {
    *  numNodes and numEdges.
    */
   function createGraph() {
-
-    var node = new Node(0);
-    node.data.title = "This is node " + node.id;
-    graph.addNode(node);
-    drawNode(node);
-
-    var nodes = [];
-    nodes.push(node);
-
-    var steps = 1;
-    while(nodes.length != 0 && steps < that.nodes_count) {
-      var node = nodes.shift();
-
-      var numEdges = randomFromTo(1, that.edges_count);
-      for(var i=1; i <= numEdges; i++) {
-        var target_node = new Node(i*steps);
-        if(graph.addNode(target_node)) {
-          target_node.data.title = "This is node " + target_node.id;
-
-          drawNode(target_node);
-          nodes.push(target_node);
-          if(graph.addEdge(node, target_node)) {
-            drawEdge(node, target_node);
-          }
-        }
-      }
-      steps++;
-    }
+      
+//    var node = new Node(0);
+//    node.data.title = "This is node " + node.id;
+//    graph.addNode(node);
+//    drawNode(node);
+//    
+//    var nodes = [];
+//    nodes.push(node);
+//    
+//    var steps = 1;
+//    while(nodes.length != 0 && steps < that.nodes_count) {
+//      var node = nodes.shift();
+//    
+//      var numEdges = randomFromTo(1, that.edges_count);
+//      for(var i=1; i <= numEdges; i++) {
+//        var target_node = new Node(i*steps);
+//        if(graph.addNode(target_node)) {
+//          target_node.data.title = "This is node " + target_node.id;
+//    
+//          drawNode(target_node);
+//          nodes.push(target_node);
+//          if(graph.addEdge(node, target_node)) {
+//            drawEdge(node, target_node);
+//          }
+//        }
+//      }
+//      steps++;
+//    }
+		
+		var nodesData = networkData.nodes;
+		
+		var nodeMap = {};
+		var node;
+		
+		groupColors = {};
+		var groupIndex = 0;
+		nodesData.forEach(function(nodeData){
+			node = new Node(nodeData.id);
+			node.data.title = nodeData.label;
+			if(nodeData.group){
+				node.data.group = nodeData.group;	
+				if(nodeData.group && !groupColors[nodeData.group]){
+					groupColors[nodeData.group] = colorPalette[groupIndex].color.background;
+					groupIndex++;
+				}
+			}
+				
+			graph.addNode(node);
+			drawNode(node);
+			nodeMap[nodeData.id] = node;
+		});
+		
+		var edgesData = networkData.edges;
+		
+		edgesData.forEach(function(edgeData){
+			if(nodeMap[edgeData.from] && nodeMap[edgeData.to]){
+				if(graph.addEdge(nodeMap[edgeData.from], nodeMap[edgeData.to])) {
+					drawEdge(nodeMap[edgeData.from], nodeMap[edgeData.to]);
+				}
+			}
+		});
 
     that.layout_options.width = that.layout_options.width || 2000;
     that.layout_options.height = that.layout_options.height || 2000;
@@ -195,7 +256,10 @@ Drawing.SimpleGraph = function(options) {
    *  Create a node object and add it to the scene.
    */
   function drawNode(node) {
-    var draw_object = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {  color: Math.random() * 0xffffff, opacity: 0.5 } ) );
+		
+    //var draw_object = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {  color: Math.random() * 0xffffff, opacity: 0.5 } ) );
+		var color = groupColors[node.data.group] ? groupColors[node.data.group] : "#97C2FC";
+		var draw_object = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( {  color: color, opacity: 1 } ) );
 
     if(that.show_labels) {
       if(node.data.title != undefined) {
@@ -226,7 +290,8 @@ Drawing.SimpleGraph = function(options) {
    *  Create an edge object (line) and add it to the scene.
    */
   function drawEdge(source, target) {
-      material = new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1, linewidth: 0.5 });
+      //material = new THREE.LineBasicMaterial({ color: 0xff0000, opacity: 1, linewidth: 0.5 });
+			material = new THREE.LineBasicMaterial({ color: 0xD3D3D3, opacity: 1, linewidth: 0.5 });
 
       var tmp_geo = new THREE.Geometry();
       tmp_geo.vertices.push(source.data.draw_object.position);
@@ -275,7 +340,8 @@ Drawing.SimpleGraph = function(options) {
         var node = graph.nodes[i];
         if(node.data.label_object != undefined) {
           node.data.label_object.position.x = node.data.draw_object.position.x;
-          node.data.label_object.position.y = node.data.draw_object.position.y - 100;
+//          node.data.label_object.position.y = node.data.draw_object.position.y - 100;
+          node.data.label_object.position.y = node.data.draw_object.position.y - 150;
           node.data.label_object.position.z = node.data.draw_object.position.z;
           node.data.label_object.lookAt(camera.position);
         } else {
